2e714021ca5297353ab70b7fec67499aa699d777
andresazuim <andresazuim37@gmail.com>
2024-03-04 20:08:44 -0300

    inserindo dados no banco de dados atraves do formulario

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"loja/models"
 	"net/http"
+	"strconv"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -14,4 +16,30 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+		http.Redirect(w, r, "/", 301)
+	}
 }
