d4c5875477647ad63ae6ff97bdf03b3aef1058ba
andresazuim <andresazuim37@gmail.com>
2024-03-04 19:19:03 -0300

    organizando a arquitetura para MVC

--- main.go
+++ main.go
@@ -1,72 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"html/template"
+	"loja/routes"
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
 
-func conectaComBancoDeDados() *sql.DB {
-
-	conexao := "user=postgres dbname=Loja password=12131316 host=localhost sslmode=disable"
-
-	db, err := sql.Open("postgres", conexao)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
-}
-
-type Produto struct {
-	Nome, Descricao string
-	Preco           float64
-	Quantidade      int
-}
-
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-
-	http.HandleFunc("/", index)
+	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-
-	db := conectaComBancoDeDados()
-
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "index", produtos)
-
-	defer db.Close()
 }
