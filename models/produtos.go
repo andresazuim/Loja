814546972be09b3cc4990f2a6dc21af9c0cc6b85
andresazuim <andresazuim37@gmail.com>
2024-03-04 20:43:12 -0300

    implementação do botao deletar, removendo o produto do banco de dados

--- models/produtos.go
+++ models/produtos.go
@@ -5,7 +5,7 @@ import "loja/db"
 type Produto struct {
 	Nome, Descricao string
 	Preco           float64
-	Quantidade      int
+	Quantidade, Id  int
 }
 
 func BuscaTodosOsProdutos() []Produto {
@@ -31,6 +31,7 @@ func BuscaTodosOsProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -51,5 +52,16 @@ func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	}
 	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
 
+	defer db.Close()
+}
+
+func DeletarProduto(id string) {
+	db := db.ConectaComBancoDeDados()
+
+	deletarOProduto, err := db.Prepare("delete from produtos where id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarOProduto.Exec(id)
 	defer db.Close()
 }
