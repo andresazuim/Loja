a2a22a2b5cc1a9006b3fb16df1c98ce1e8e94928
andresazuim <andresazuim37@gmail.com>
2024-03-04 22:50:39 -0300

    implementação do botao editar e suas funcionalidades

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -49,3 +49,37 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	models.DeletarProduto(idDoProduto)
 	http.Redirect(w, r, "/", 301)
 }
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idDoProduto := r.URL.Query().Get("id")
+	produto := models.EditaProduto(idDoProduto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idConvertidaParaInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na converão do ID para int:", err)
+		}
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na converão do preço para int:", err)
+		}
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na converão da quantidade para int:", err)
+		}
+
+		models.AtualizaProduto(idConvertidaParaInt, nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+
+	}
+	http.Redirect(w, r, "/", 301)
+}
